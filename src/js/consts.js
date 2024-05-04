ed60580bf5f9c3f37c759e562d455937c8b96c63
Denischehov <ivanhram511@gmail.com>
2024-05-05 06:06:12 +0800

    first commit

---
+++ src/js/consts.js
@@ -0,0 +1,28 @@
+export const WORDS = [
+	'apple',
+	'bee',
+	'cat',
+	'dog',
+	'elephant',
+	'fox',
+	'giraffe',
+	'hippopotamus',
+	'igloo',
+	'jaguar',
+	'kangaroo',
+	'lemon',
+	'monkey',
+	'octopus',
+	'penguin',
+	'queen',
+	'rabbit',
+	'sun',
+	'tiger',
+	'unicorn',
+	'vulture',
+	'whale',
+	'yoga',
+	'zebra',
+]
+
+export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
